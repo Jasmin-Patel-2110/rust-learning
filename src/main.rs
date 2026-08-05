aa1e1b426ac36954acd1f9dafb2373d0f6462cc9
Jasmin Patel <jasmin.patel2110@gmail.com>
2026-08-05 18:41:27 +0530

    learn: practice loops and string iteration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let sentence: String = String::from("My name is Jasmin.");
+    let first_word: String = get_first_word(sentence);
+
+    let n = 100;
+    for i in 0..n {
+        println!("number: {i}");
+    }
+
+    println!("First word is {first_word}");
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans: String = String::new();
+
+    for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    }
+
+    return ans;
 }
